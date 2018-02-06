d7c40e22696f7049293e1181c2909b9c47014b10
Janis <janis@jaggedverge.com>
2018-02-06 21:30:03 +1100

    Include thread info in handler

--- excepthook_example.py
+++ excepthook_example.py
@@ -7,19 +7,26 @@ from threaded_exception import RunsInAThread
 logger = logging.getLogger(__name__)
 logging.basicConfig(filename='example.log', filemode='w', level=logging.DEBUG)
 
-def handle_unhandled_exception(exc_type, exc_value, exc_traceback):
+def handle_unhandled_exception(exc_type, exc_value, exc_traceback, thread_identifier=''):
     """Handler for unhandled exceptions that will write to the logs"""
     if issubclass(exc_type, KeyboardInterrupt):
         # call the default excepthook saved at __excepthook__
         sys.__excepthook__(exc_type, exc_value, exc_traceback)
         return
-    logger.critical("Unhandled exception", exc_info=(exc_type, exc_value, exc_traceback))
+
+    if not thread_identifier:
+        logger.critical("Unhandled exception", exc_info=(exc_type, exc_value, exc_traceback))
+    else:
+        logger.critical("Unhandled exception (on thread %s)", thread_identifier, exc_info=(exc_type, exc_value, exc_traceback))
+        
 
 sys.excepthook = handle_unhandled_exception
 
 
 def patch_threading_excepthook():
-    """Installs our exception handler into the threading"""
+    """Installs our exception handler into the threading
+    Inspired by https://bugs.python.org/issue1230540
+    """
     old_init = threading.Thread.__init__
     def new_init(self, *args, **kwargs):
         old_init(self, *args, **kwargs)
@@ -30,7 +37,7 @@ def patch_threading_excepthook():
             except (KeyboardInterrupt, SystemExit):
                 raise
             except:
-                sys.excepthook(*sys.exc_info())
+                sys.excepthook(*sys.exc_info(), thread_identifier=threading.get_ident())
         self.run = run_with_our_excepthook
     threading.Thread.__init__ = new_init
 
